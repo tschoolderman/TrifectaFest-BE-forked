127e22e2baa1132966632fd6ce596c5ceafc11e3
Timo_Bos <t.bos.1998@gmail.com>
2023-03-29 10:06:23 +0200

    Added person

--- demo/src/main/java/com/example/demo/rest/PersonEndpoint.java
+++ demo/src/main/java/com/example/demo/rest/PersonEndpoint.java
@@ -1,5 +1,21 @@
 package com.example.demo.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.persistance.PersonService;
+
+@RestController
 public class PersonEndpoint {
 
+	@Autowired
+	PersonService service;
+	
+	@GetMapping("Saveperson")
+	public void save() {
+		System.out.println("we zijn in het endpoint");
+		service.Save();
+	}
+	
 }
