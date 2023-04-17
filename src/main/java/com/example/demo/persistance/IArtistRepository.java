9475b780734f86af4a06f36a7723b7a86ae54a8e
Zenzorido <rd6@live.nl>
2023-04-17 14:49:18 +0200

    Werkend

--- src/main/java/com/example/demo/persistance/IArtistRepository.java
+++ src/main/java/com/example/demo/persistance/IArtistRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.persistance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import com.example.demo.entiteien.Artist;
 
+@Component
 public interface IArtistRepository extends JpaRepository<Artist, Long> {
 
 	
