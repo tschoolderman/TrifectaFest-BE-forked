65bfdb78353c3f8244d89d3445931aa766cf4709
Zenzorido <rd6@live.nl>
2023-04-17 15:11:29 +0200

    Filter booking request

--- src/main/java/com/example/demo/persistance/IBookingRequestRepository.java
+++ src/main/java/com/example/demo/persistance/IBookingRequestRepository.java
@@ -2,11 +2,14 @@ package com.example.demo.persistance;
 
 import com.example.demo.entiteien.*;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public interface IBookingRequestRepository extends JpaRepository<BookingRequest , Long > {
+	List<BookingRequest> findByArtist(Artist artist);
 
 }
